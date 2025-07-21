e07b2434c61ab33f7d6b52f08e6408252f1ba406
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:51:56 +0530

    Add  src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/SenderTypeFMA_1.java

---
+++ src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/SenderTypeFMA_1.java
@@ -0,0 +1,13 @@
+package com.yourorg.fetchmessagesapi.model.enums.fma1;
+
+/**
+ * Enumeration for the type of sender.
+ */
+public enum SenderTypeFMA_1 {
+    USER,
+    SYSTEM,
+    BOT
+}
+```
+```java
+// src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/MessageContentTypeFMA_1.java
