6e5c235b5c1de92611f2ee9eec6a4b9ca0166d10
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:51:57 +0530

    Add  src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/MessageContentTypeFMA_1.java

---
+++ src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/MessageContentTypeFMA_1.java
@@ -0,0 +1,13 @@
+package com.yourorg.fetchmessagesapi.model.enums.fma1;
+
+/**
+ * Enumeration for the type of message content.
+ */
+public enum MessageContentTypeFMA_1 {
+    TEXT,
+    IMAGE,
+    FILE
+}
+```
+```java
+// src/main/java/com/yourorg/fetchmessagesapi/model/enums/fma1/ErrorCodeFMA_1.java
