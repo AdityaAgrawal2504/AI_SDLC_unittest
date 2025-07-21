a3b73946b3932ae7d46cd9d067ea0a428b97117a
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:22:24 +0530

    Add  src/main/java/com/auth/api/UserRegistrationApiApplication_33A1.java

---
+++ src/main/java/com/auth/api/UserRegistrationApiApplication_33A1.java
@@ -0,0 +1,18 @@
+package com.auth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class UserRegistrationApiApplication_33A1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApiApplication_33A1.class, args);
+    }
+
+}
+```
+```java
+// src/main/java/com/auth/api/constants/UserRegAPI_ValidationConstants_33A1.java
