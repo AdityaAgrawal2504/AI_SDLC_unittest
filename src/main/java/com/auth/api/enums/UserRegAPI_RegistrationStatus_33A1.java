1e96c7b9d646c9ea5e36515d2c14bbcd633981af
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:22:25 +0530

    Add  src/main/java/com/auth/api/enums/UserRegAPI_RegistrationStatus_33A1.java

---
+++ src/main/java/com/auth/api/enums/UserRegAPI_RegistrationStatus_33A1.java
@@ -0,0 +1,11 @@
+package com.auth.api.enums;
+
+/**
+ * Defines the status of the user registration process.
+ */
+public enum UserRegAPI_RegistrationStatus_33A1 {
+    REGISTRATION_SUCCESSFUL
+}
+```
+```java
+// src/main/java/com/auth/api/dto/UserRegAPI_UserRegistrationRequest_33A1.java
