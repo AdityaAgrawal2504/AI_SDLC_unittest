a59d431140be783b1ba51a4d2fa18dfbb1e4143e
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:23:05 +0530

    Add  src/main/java/com/fetchconversations/api/FetchConversationsApiApplicationFCA911.java

---
+++ src/main/java/com/fetchconversations/api/FetchConversationsApiApplicationFCA911.java
@@ -0,0 +1,16 @@
+package com.fetchconversations.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchConversationsApiApplicationFCA911 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchConversationsApiApplicationFCA911.class, args);
+    }
+
+}
+```
+```java
+// src/main/java/com/fetchconversations/api/logging/StructuredLoggerFCA911.java
