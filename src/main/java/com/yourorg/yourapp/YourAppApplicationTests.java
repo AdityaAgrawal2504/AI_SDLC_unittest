a9a8f2fcaea01b23cfa49c4ecf4b216c4e443eb3
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:51:31 +0530

    Add  src/main/java/com/yourorg/yourapp/YourAppApplicationTests.java

---
+++ src/main/java/com/yourorg/yourapp/YourAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yourorg.yourapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+```
