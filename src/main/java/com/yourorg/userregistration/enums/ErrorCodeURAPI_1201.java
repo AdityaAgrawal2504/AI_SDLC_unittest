12a0b3c50a59510861667e4eb40108472f1900c8
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-21 17:51:10 +0530

    Add  src/main/java/com/yourorg/userregistration/enums/ErrorCodeURAPI_1201.java

---
+++ src/main/java/com/yourorg/userregistration/enums/ErrorCodeURAPI_1201.java
@@ -0,0 +1,13 @@
+package com.yourorg.userregistration.enums;
+
+/**
+ * Enumeration of standardized error codes for the API.
+ */
+public enum ErrorCodeURAPI_1201 {
+    VALIDATION_ERROR,
+    USER_ALREADY_EXISTS,
+    INTERNAL_SERVER_ERROR
+}
+```
+```java
+// src/main/java/com/yourorg/userregistration/exception/AbstractRestExceptionURAPI_1201.java
