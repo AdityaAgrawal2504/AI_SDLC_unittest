32cd0237baeacde89960df9746ba0dfaba2cbe14
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-22 12:02:59 +0530

    Add  src/main/java/com/example/usersearch/security/AuthenticatedUser_A0B1.java

---
+++ src/main/java/com/example/usersearch/security/AuthenticatedUser_A0B1.java
@@ -0,0 +1,58 @@
+package com.example.usersearch.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticatedUser_A0B1 implements UserDetails {
+
+    private final UUID id;
+    private final String name; // User's full name
+    private final String phoneNumber; // User's phone number (used as username for auth)
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+```
+src/main/java/com/example/usersearch/security/JwtUtil_A0B1.java
+```java
