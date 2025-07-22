7c528403939cdad0c60a33d6cf1ce7e7363c79e2
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-22 12:02:56 +0530

    Add  src/main/java/com/example/usersearch/repository/BlockedUserRepository_A0B1.java

---
+++ src/main/java/com/example/usersearch/repository/BlockedUserRepository_A0B1.java
@@ -0,0 +1,13 @@
+package com.example.usersearch.repository;
+
+import com.example.usersearch.model.BlockedUserEntity_A0B1;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface BlockedUserRepository_A0B1 extends JpaRepository<BlockedUserEntity_A0B1, BlockedUserEntity_A0B1.BlockedUserPK> {
+}
+```
+src/main/java/com/example/usersearch/logging/Loggable_A0B1.java
+```java
