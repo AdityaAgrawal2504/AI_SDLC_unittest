ed49cd9f304f677ee4cd26e51292d5fd08e54469
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-22 12:01:45 +0530

    Add  src/main/java/com/example/userregistration/UserRegistrationApiApplication_URAPI_1.java

---
+++ src/main/java/com/example/userregistration/UserRegistrationApiApplication_URAPI_1.java
@@ -0,0 +1,18 @@
+package com.example.userregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class UserRegistrationApiApplication_URAPI_1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApiApplication_URAPI_1.class, args);
+    }
+
+}
+```
+src/main/java/com/example/userregistration/model/ErrorCode_URAPI_1.java
+```java
