e0b45be96e355085b186be48303598429cfb4f8c
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-24 11:53:58 +0530

    Delete src/main/java/com/example directory

--- src/main/java/com/example/provider/interfaces/IPasswordHasher_A1B2C.java
+++
@@ -1,7 +0,0 @@
-src/main/java/com/example/provider/interfaces/IPasswordHasher_A1B2C.java
-package com.example.provider.interfaces;
-
-public interface IPasswordHasher_A1B2C {
-    String hash(String password);
-    boolean matches(String rawPassword, String encodedPassword);
-}
