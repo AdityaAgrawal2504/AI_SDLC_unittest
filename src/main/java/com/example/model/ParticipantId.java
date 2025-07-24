d82c66da549a4e739e88319b4c7eaf6c2e9e636c
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-24 12:10:01 +0530

    Add  src/main/java/com/example/model/ParticipantId.java

---
+++ src/main/java/com/example/model/ParticipantId.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantId implements Serializable {
+
+    private UUID userId;
+    private UUID conversationId;
+
+}
