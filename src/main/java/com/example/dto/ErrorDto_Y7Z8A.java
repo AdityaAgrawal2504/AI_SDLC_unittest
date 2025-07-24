c05702d343f53774454d9c4bffb6df5a7e981b9c
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-24 11:46:56 +0530

    Add  src/main/java/com/example/dto/ErrorDto_Y7Z8A.java

---
+++ src/main/java/com/example/dto/ErrorDto_Y7Z8A.java
@@ -0,0 +1,15 @@
+src/main/java/com/example/dto/ErrorDto_Y7Z8A.java
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto_Y7Z8A {
+    private int statusCode;
+    private String message;
+    private String error;
+}
