81a991985eef563bd7c2488cdd7ffc1eace9b860
AdityaAgrawal2504 <119430452+AdityaAgrawal2504@users.noreply.github.com>
2025-07-29 14:55:39 +0530

    Add  src/main/java/com/example/dto/PaginationInfo.java

---
+++ src/main/java/com/example/dto/PaginationInfo.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginationInfo {
+    private int currentPage;
+    private int pageSize;
+    private long totalItems;
+    private int totalPages;
+}
